439b73a5a462e41d4e2d4ee2d5aadd3e4faa3724
Sankalp Nadiger <128134398+sankalp-nadiger@users.noreply.github.com>
2025-09-01 12:05:05 +0530

    Update 7.py

--- ML_LAB/7.py
+++ ML_LAB/7.py
@@ -11,15 +11,15 @@ yg = glass_data['Type'].values
 distance_metrics= ['manhattan','euclidean']
 
 for p,distance_metric in enumerate(distance_metrics):
-            for ts in test_splits:
-                print(f"K={k},Distance Metric={distance_metric}: ")
-                print(f"Split = {ts}")
-                knn=KNN(n_neighbors=3,p=p+1,weights="distance")
-                if dataset == 'Glass':
+            print(f"Distance Metric={distance_metric}: ")
+            knn=KNN(n_neighbors=3,p=p+1,weights="distance")
+            if dataset == 'Glass':
                     X_train,X_test, y_train,y_test=train_test_split(Xg,yg,test_size=0.3,random_state=42)
-                else:
+            else:
                     X_train,X_test, y_train,y_test=train_test_split(Xf,yf,test_size=0.3,random_state=42)
-                knn.fit(X_train,y_train)
-                accuracy=knn.score(X_test,y_test)
-                print(f"accuracy on test set: {accuracy:.2f}")
-                print("------------")
+            knn.fit(X_train,y_train)
+            accuracy=knn.score(X_test,y_test)
+            print(f"accuracy on test set: {accuracy:.2f}")
+            print("------------")
+
+
